60572e4a38fd40c14c51566cfbdb965fa977a3a2
Renato Dehnhardt <renatotkd@gmail.com>
2025-10-29 08:33:28 +0100

    chore: remove `laravel-connector-react` package and related files
    - Delete context, hooks, and types specific to `laravel-connector-react`.
    - Remove associated tests, scripts, dependencies, and package metadata.

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export {ApiProvider, useApiContext} from './context/api-provider'
-export {useMutation} from './hooks/use-mutation'
-export {useApi} from './hooks/use-api'
-
-export type {UseApiOptions, UseApiResult, UseMutationOptions, UseMutationResult, ApiProviderProps} from './types'
